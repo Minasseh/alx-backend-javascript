de9b620ef312d2ec2b2177a1e1642b8a87296952
Minasseh <mnsalaro@yahoo.com>
2024-04-30 10:16:16 +0300

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(arrayStudents) {
   if (!Array.isArray(arrayStudents)) {
     return [];
   }
-  return arrayStudents.map(obj => obj['id']);
+  return arrayStudents.map(obj => obj.id);
 }
