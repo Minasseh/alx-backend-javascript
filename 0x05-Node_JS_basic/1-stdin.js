797b0d6453585538e13e8ea7992c6847a23d47f6
Minasseh <mnsalaro@yahoo.com>
2024-08-20 12:25:47 +0300

    Corrections

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,14 @@
 process.stdin.setEncoding('utf8');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n')
-process.stdin.on('data', function(data) {
-  var name = data.trim();
-  if (name !== null) {
-    process.stdout.write('Your name is: ' + name + '\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write('Your name is: ' + chunk);
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
