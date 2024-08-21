9e909f8206daf95c446e56b158d2a0b383853f1e
Minasseh <mnsalaro@yahoo.com>
2024-08-21 22:39:10 +0300

    Modify

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,8 +4,14 @@ const calculateNumber = require("./0-calcul.js");
 describe('calculateNumber', () => {
   it('adds two numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('adds two numbers', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('adds two numbers', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('adds two numbers', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
 });
 });
